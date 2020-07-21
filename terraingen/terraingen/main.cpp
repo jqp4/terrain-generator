e79aca5669c547c8fd69338473fbeebd64a38c9a
Gleb <skryabin.gleb.d@gmail.com>
2020-07-21 12:49:01 +0300

    add drawing

--- terraingen/terraingen/main.cpp
+++ terraingen/terraingen/main.cpp
@@ -15,14 +15,190 @@
 
 const int winWidth  = 2100;
 const int winHeight = 1400;
-const int xLen = 13; // ---
-const int yLen = 8;  //  |
-const int world_height = 10;
-const int octaveAmount = (int)log2(xLen < yLen ? xLen : yLen);
+const int xLen = 90; // ---
+const int yLen = 65; //  |
+const sf::Color bgColor = sf::Color::White;
+const sf::Color frameColor = sf::Color(100, 100, 100);
 
 
-
-
+class WindowField{
+public:
+    int pixel_size;
+    int indent;
+    int olt; //OutlineThickness
+    
+    class BackgroundPicture{
+    public:
+        static const int qn = 1;
+        sf::RectangleShape quads[qn];
+        
+        BackgroundPicture(){}
+        
+        BackgroundPicture(int pixel_size, int indent){
+            quads[0].setSize(sf::Vector2f(pixel_size * xLen, pixel_size * yLen));
+            quads[0].setPosition(indent, indent);
+            quads[0].setOutlineThickness(indent);
+            quads[0].setOutlineColor(frameColor);
+            quads[0].setFillColor(bgColor);
+            
+            /*quads[0].setSize(sf::Vector2f(width, height));
+            quads[0].setPosition(0, 0);
+            quads[0].setFillColor(frameColor);
+            
+            quads[1].setSize(sf::Vector2f(pixel_size * xLen, pixel_size * yLen));
+            quads[1].setPosition(indent, indent);
+            quads[1].setFillColor(bgColor);*/
+        }
+        
+        void draw(sf::RenderWindow *window){
+            for (int i = 0; i < qn; i++){
+                window->draw(quads[i]);
+            }
+        }
+    } bgpic;
+    
+    class Landscape{
+    public:
+        int octaveAmount = log2(xLen < yLen ? xLen : yLen);
+        int octaves[10][yLen][xLen]; // xLen, yLen < 2^10 = 1024
+        int field[yLen][xLen];
+        int height = 40;
+        
+        Landscape(){
+            for (int onum = 0; onum < octaveAmount; onum++){
+                int tfx = 0, step = pow(2, onum);
+                for (int y = 0; y < yLen; y += step){
+                    for (int x = 0; x < xLen; x += step){
+                        tfx = (int)(rand() % height);
+                        int xm = (x + step) > xLen ? xLen : (x + step);
+                        int ym = (y + step) > yLen ? yLen : (y + step);
+                        for (int j = y; j < ym; j++){
+                            for (int i = x; i < xm; i++){
+                                octaves[onum][j][i] = tfx;
+                            }
+                        }
+                    }
+                }
+            }
+            for (int y = 0; y < yLen; y++){
+                for (int x = 0; x < xLen; x++){
+                    int sum = 0;
+                    for (int onum = 0; onum < octaveAmount; onum++){
+                        sum += octaves[onum][y][x];
+                    }
+                    field[y][x] = sum / octaveAmount;
+                }
+            }
+        }
+        
+        void showOctaves(){
+            for (int onum = 0; onum < octaveAmount; onum++){
+                for (int y = 0; y < yLen; y++){
+                    for (int x = 0; x < xLen; x++){
+                        printf("%2.1d ", octaves[onum][y][x]);
+                    }
+                    printf("\n");
+                }
+                printf("\n");
+            }
+        }
+        
+        void showField(){
+            for (int y = 0; y < yLen; y++){
+                for (int x = 0; x < xLen; x++){
+                    printf("%2.1d ", field[y][x]);
+                }
+                printf("\n");
+            }
+        }
+        
+    } landscape;
+    
+    class Ghost{
+    public:
+        sf::RectangleShape bot;
+        sf::RectangleShape surface;
+        sf::RectangleShape organics;
+        sf::ConvexShape custom;
+        
+        Ghost(){}
+        
+        Ghost(int ps, int olt){
+            bot = sf::RectangleShape(sf::Vector2f(ps - olt*2, ps - olt*2));
+            bot.setOutlineColor(sf::Color(82, 82, 82));
+            bot.setOutlineThickness(olt);
+            
+            surface = sf::RectangleShape(sf::Vector2f(ps, ps));
+            
+            organics = sf::RectangleShape(sf::Vector2f(ps - olt*2, ps - olt*2));
+            organics.setFillColor(sf::Color(202, 202, 202));
+            organics.setOutlineColor(bgColor);
+            organics.setOutlineThickness(olt);
+            
+            custom.setPointCount(3);
+            custom.setPoint(0, sf::Vector2f(ps/2, 0));
+            custom.setPoint(1, sf::Vector2f(ps, ps));
+            custom.setPoint(2, sf::Vector2f(0, ps));
+            custom.setFillColor(sf::Color::Black);
+        }
+    } ghost;
+    
+    WindowField(int pixel_size, int outline_thickness){
+        this->pixel_size = pixel_size;
+        olt = outline_thickness;
+        indent = pixel_size/2;
+        bgpic = BackgroundPicture(pixel_size, indent);
+        ghost = Ghost(pixel_size, olt);
+    }
+    
+    void draw(sf::RenderWindow *window){
+        bgpic.draw(window);
+        for (int x = 0; x < xLen; x++){
+            for (int y = 0; y < yLen; y++){
+                //int i = world.field[x][y];
+                int wx = indent + olt + pixel_size * x;
+                int wy = indent + olt + pixel_size * y;
+                /*switch (i){
+                    case -1:
+                        //pass
+                        break;
+                    case -2:
+                        //organics
+                        ghost.organics.setPosition(wx, wy);
+                        window->draw(ghost.organics);
+                        break;
+                    default:
+                        //bot
+                        if (i < 0){
+                            //World::Bot bbb = world.bots[i]; // debug custom bot:
+                            ghost.custom.setPosition(wx, wy);
+                            //ghost.custom.setFillColor(world.bots[i].color);
+                            window->draw(ghost.custom);
+                        } else {
+                            ghost.bot.setPosition(wx, wy);
+                            //ghost.bot.setFillColor(world.bots[i].color);
+                            ghost.bot.setFillColor(world.bots[i].getColor());
+                            window->draw(ghost.bot);
+                        }
+                        if (debug == 2){
+                            text.setString(std::to_string(world.bots[i].energy));
+                            text.setFillColor(sf::Color::Black);
+                            text.setPosition(wx - 2, wy - 9);
+                            window->draw(text);
+                        }
+                        break;
+                }*/
+                
+                //case -1:
+                //surface
+                ghost.surface.setPosition(wx - olt, wy - olt);
+                int tbc = (float)landscape.field[y][x] / landscape.height * 255;
+                ghost.surface.setFillColor(sf::Color(0, 0, tbc));
+                window->draw(ghost.surface);
+            }
+        }
+    }
+};
 
 
 int main(){
@@ -32,63 +208,7 @@ int main(){
     srand(static_cast<unsigned int>(time(nullptr)));
     unsigned int seed = rand();
     
-    //int octaves[octaveAmount][xLen][yLen];
-    //int landscape[xLen][yLen];
-    int octaves[octaveAmount][yLen][xLen];
-    int landscape[yLen][xLen];
-    
-    for (int onum = 0; onum < octaveAmount; onum++){
-        int tfx = 0, step = pow(2, onum);
-        for (int y = 0; y < yLen; y += step){
-            for (int x = 0; x < xLen; x += step){
-                tfx = (int)(rand() % world_height);
-                int xm = (x + step) > xLen ? xLen : (x + step);
-                int ym = (y + step) > yLen ? yLen : (y + step);
-                for (int j = y; j < ym; j++){
-                    for (int i = x; i < xm; i++){
-                        octaves[onum][j][i] = tfx;
-                    }
-                }
-            }
-        }
-    }
-    
-    for (int onum = 0; onum < octaveAmount; onum++){
-        for (int y = 0; y < yLen; y++){
-            for (int x = 0; x < xLen; x++){
-                printf("%2.1d ", octaves[onum][y][x]);
-            }
-            printf("\n");
-        }
-        printf("\n");
-    }
-    
-    for (int y = 0; y < yLen; y++){
-        for (int x = 0; x < xLen; x++){
-            int sum = 0;
-            for (int onum = 0; onum < octaveAmount; onum++){
-                sum += octaves[onum][y][x];
-            }
-            landscape[y][x] = sum / octaveAmount;
-        }
-    }
-    
-    for (int y = 0; y < yLen; y++){
-        for (int x = 0; x < xLen; x++){
-            printf("%2.1d ", landscape[y][x]);
-        }
-        printf("\n");
-    }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    window.close();
+    WindowField winfld(20, 2);
     
     while(window.isOpen()){
         sf::Event event;
@@ -120,6 +240,7 @@ int main(){
         }
         
         window.clear();
+        winfld.draw(&window);
         window.display();
         sf::sleep(sf::milliseconds(10));
     }
